c4f5e1793eb7487efeb20781ef40df8828f64855
FabianVarela <jfabian.303@gmail.com>
2022-10-03 13:09:59 -0500

    Create mocks for unit test

---
+++ test/mocks/mocks.dart
@@ -0,0 +1,8 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockRequestContext extends Mock implements RequestContext {}
+
+class MockRequest extends Mock implements Request {}
+
+class MockUri extends Mock implements Uri {}
