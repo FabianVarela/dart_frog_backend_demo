89bb3bff72b5d557080e0a961f88d56838966e62
FabianVarela <jfabian.303@gmail.com>
2022-10-03 13:14:28 -0500

    Update index.dart for route initial and their tests

--- test/routes/index_test.dart
+++ test/routes/index_test.dart
@@ -5,19 +5,85 @@ import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import '../../routes/index.dart' as route;
-
-class _MockRequestContext extends Mock implements RequestContext {}
+import '../mocks/mocks.dart';
 
 void main() {
+  late RequestContext mockContext;
+  late Request mockRequest;
+  late Uri mockUri;
+
+  setUp(() {
+    mockContext = MockRequestContext();
+    mockRequest = MockRequest();
+    mockUri = MockUri();
+
+    // Mock request from context
+    when(() => mockContext.request).thenReturn(mockRequest);
+
+    // Mock Uri
+    when(() => mockRequest.uri).thenReturn(mockUri);
+    when(() => mockUri.resolve(any())).thenAnswer(
+      (_) => Uri.parse('http://localhost:8080/'),
+    );
+  });
+
   group('GET /', () {
-    test('responds with a 200 and "Welcome to Dart Frog!".', () {
-      final context = _MockRequestContext();
-      final response = route.onRequest(context);
+    test('responds with a 200 and greeting.', () async {
+      // Arrange
+      const greeting = 'Welcome to the Dart Frog Demo';
+
+      when(() => mockContext.read<String>()).thenReturn(greeting);
+      when(() => mockRequest.method).thenReturn(HttpMethod.get);
+
+      // Act
+      final response = route.onRequest(mockContext);
+
+      // Assert
       expect(response.statusCode, equals(HttpStatus.ok));
-      expect(
-        response.body(),
-        completion(equals('Welcome to Dart Frog!')),
-      );
+      expect(response.body(), completion(equals(greeting)));
+    });
+  });
+
+  group('Error 405', () {
+    test('Return 405 if method is POST', () {
+      // Arrange
+      when(() => mockRequest.method).thenReturn(HttpMethod.post);
+
+      // Act
+      final response = route.onRequest(mockContext);
+
+      // Assert
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
+    });
+    test('Return 405 if method is PUT', () {
+      // Arrange
+      when(() => mockRequest.method).thenReturn(HttpMethod.put);
+
+      // Act
+      final response = route.onRequest(mockContext);
+
+      // Assert
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
+    });
+    test('Return 405 if method is PATCH', () {
+      // Arrange
+      when(() => mockRequest.method).thenReturn(HttpMethod.patch);
+
+      // Act
+      final response = route.onRequest(mockContext);
+
+      // Assert
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
+    });
+    test('Return 405 if method is DELETE', () {
+      // Arrange
+      when(() => mockRequest.method).thenReturn(HttpMethod.delete);
+
+      // Act
+      final response = route.onRequest(mockContext);
+
+      // Assert
+      expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
     });
   });
 }
