66c49e546a1fe3df367c8e10e1c9347d3a57f66f
FabianVarela <jfabian.303@gmail.com>
2022-10-04 12:42:31 -0500

    Add routes by / and /id

--- routes/json/index.dart
+++ routes/json/index.dart
@@ -1,22 +1,57 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_backend_demo/model/address_model.dart';
 import 'package:dart_frog_backend_demo/model/user_model.dart';
 
-import '../_middleware.dart';
+/*
+curl --request GET --url http://localhost:8080/json
+curl --request POST --url http://localhost:8080/json \
+  --data '{"name": "Argel B.", "age": 30}'
+*/
 
-Response onRequest(RequestContext context) {
+FutureOr<Response> onRequest(RequestContext context) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context);
+    case HttpMethod.post:
+      return _post(context);
+    default:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _get(RequestContext context) async {
   final serverMessage = context.read<String>();
   final address = context.read<AddressModel>();
 
-  final greeting = context.read<MyString>();
-  print(greeting);
+  return Response.json(
+    body: [
+      UserModel(
+        name: 'Dash',
+        age: 42,
+        serverMessage: serverMessage,
+        address: address,
+      ),
+    ],
+  );
+}
+
+Future<Response> _post(RequestContext context) async {
+  final serverMessage = context.read<String>();
+  final address = context.read<AddressModel>();
+
+  final request = context.request;
+
+  final userModel = UserModel.fromJson({
+    ...await request.json(),
+    'serverMessage': serverMessage,
+    'address': address.toJson(),
+  });
 
   return Response.json(
-    body: UserModel(
-      name: 'Dash',
-      age: 42,
-      serverMessage: serverMessage,
-      address: address,
-    ),
+    statusCode: HttpStatus.created,
+    body: {'message': 'ok', 'body': userModel.toJson()},
   );
 }
