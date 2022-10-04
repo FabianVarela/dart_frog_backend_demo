acec3e6196061cd708f8a5aac0386e7e1bfba4f0
FabianVarela <jfabian.303@gmail.com>
2022-10-04 12:38:50 -0500

    Add copy with method

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -15,6 +15,20 @@ class UserModel {
   factory UserModel.fromJson(Map<String, dynamic> json) =>
       _$UserModelFromJson(json);
 
+  UserModel copyWith({
+    String? name,
+    int? age,
+    String? serverMessage,
+    AddressModel? address,
+  }) {
+    return UserModel(
+      name: name ?? this.name,
+      age: age ?? this.age,
+      serverMessage: serverMessage ?? this.serverMessage,
+      address: address ?? this.address,
+    );
+  }
+
   final String name;
   final int age;
   final String serverMessage;
