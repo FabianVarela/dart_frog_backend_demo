2cb28d12e880723a6a1810549f9712a62a8e434c
FabianVarela <jfabian.303@gmail.com>
2022-10-04 12:17:28 -0500

    Add mock data

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -1,4 +1,6 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_backend_demo/model/address_model.dart';
+import 'package:dart_frog_backend_demo/model/user_model.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockRequestContext extends Mock implements RequestContext {}
@@ -6,3 +8,16 @@ class MockRequestContext extends Mock implements RequestContext {}
 class MockRequest extends Mock implements Request {}
 
 class MockUri extends Mock implements Uri {}
+
+const mockServerMessage = 'Welcome to Not The Dart Side!';
+const mockAddress = AddressModel(
+  street: 'Bogota',
+  number: 2503,
+  zipCode: 110721,
+);
+const mockUser = UserModel(
+  name: 'Dash',
+  age: 42,
+  serverMessage: mockServerMessage,
+  address: mockAddress,
+);
