d56e0b12ecf2b7c10cf12861d0972c8a62515b60
FabianVarela <Jfabian.303@gmail.com>
2025-08-23 19:08:12 -0500

    Inject cubit as global provider

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,7 +1,15 @@
 import 'dart:convert';
 
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_backend_demo/cubit/color/color_manager_cubit.dart';
 import 'package:dart_frog_backend_demo/model/address/address_model.dart';
+import 'package:dart_frog_backend_demo/websocket/websocket_handler.dart';
+
+final _colorManagerCubit = ColorManagerCubit();
+
+Middleware _colorManagerProvider() {
+  return provider<ColorManagerCubit>((_) => _colorManagerCubit);
+}
 
 Handler middleware(Handler handler) {
   return handler
@@ -24,15 +32,22 @@ Handler middleware(Handler handler) {
           return response.copyWith(body: body);
         };
       })
-      .use(provider<String>((context) => 'Welcome to Not The Dart Side!'))
+      .use(provider<String>((_) => 'Welcome to Not The Dart Side!'))
       .use(
         provider<AddressModel>(
-          (context) => const AddressModel(
+          (_) => const AddressModel(
             street: 'Bogota',
             number: 2503,
             zipCode: 110721,
           ),
         ),
       )
+      .use((handler) {
+        return (context) async {
+          WebSocketHandler.initialize(context);
+          return handler(context);
+        };
+      })
+      .use(_colorManagerProvider())
       .use(requestLogger());
 }
